f9e6dc80d361ad27a099b1db0cbc22d572ec38b7
Ari0812 <107007639+Ari8012@users.noreply.github.com>
2023-02-06 23:01:54 -0600

    Página Fauna de Costa Rica
    Inicio.
    Info Costa Rica.
    Fauna de Costa Rica.
    Mapa.

---
+++ com.Tarea2_DesarrolloWeb_Tarea2_DesarrolloWeb_jar_1/src/main/java/com/Tarea2_DesarrolloWeb/Tarea2DesarrolloWebApplication.java
@@ -0,0 +1,13 @@
+package com.Tarea2_DesarrolloWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea2DesarrolloWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea2DesarrolloWebApplication.class, args);
+	}
+
+}
